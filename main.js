9c5e3cbfd18eebebb241d30fd99eae8a8bc75a93
ahmed-dabos <ahmedghazydabos@gmail.com>
2024-12-28 11:01:22 +0200

    simple modification

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ menu.style.cursor = "pointer";
 let ul = document.getElementsByTagName("ul")[0];
 let span = document.getElementsByTagName("span")[1];
 menu.addEventListener("click", function (event) {
-  span.style.width = "100%";
-  ul.style.display = "block";
+    if(ul.style.display === "block"){
+      span.style.width = "60%";
+      ul.style.display = "none";
+    } else {
+      span.style.width = "100%";
+      ul.style.display = "block";
+    }
   event.stopPropagation();
 });
 document.addEventListener("click", function () {
